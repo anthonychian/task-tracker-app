7d441a82007712e12a55ce53ad2a6229771c1cf7
yk0610 <hwaryun0610@gmail.com>
2023-04-20 15:39:38 -0500

    only login in effect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import AllTaskPage from "./pages/AllTaskPage"
-import NewTaskPage from "./pages/NewTaskPage";
-import LoginPage from "./pages/LoginPage";
+import Login from "./components/Login";
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<LoginPage />} />
-      <Route path="/:username" element={<AllTaskPage />}/>
-      <Route path="/new-task" element={<NewTaskPage />} />
+      <Route exact path="/" element={<Login />} />
     </Routes>
   );
 }
